5ae67fb98133e37fc11f6ec70e1f9b12f59bede7
helloryantg <helloryantg@gmail.com>
2020-03-05 15:21:39 -0800

    Adds user validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,7 @@ const User = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 const HttpStatus = require('http-status-codes')
 
-const createJWT = userId => jwt.sign({ userId }, process.env.SECRET, { expiresIn: '24h' })
+const createJWT = id => jwt.sign({ id }, process.env.SECRET, { expiresIn: '24h' })
 
 router.post('/register', async (req, res) => {
   const { 
@@ -27,7 +27,25 @@ router.post('/register', async (req, res) => {
 
     res.status(HttpStatus.OK).send({ token: createJWT(newUser.id) })
   } catch (err) {
-    res.status(HttpStatus.BAD_REQUEST).send(err)
+    res.status(HttpStatus.BAD_REQUEST).send({ msg: err })
+  }
+})
+
+router.get('/validate/:token', async (req, res) => {
+  const { token } = req.params
+  
+  if (!token) {
+    res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({ msg: 'You must provide a token to authenticate' })
+  }
+
+  try {
+    const verified = await jwt.verify(token, process.env.SECRET)
+
+    console.log('id', verified)
+
+    res.status(HttpStatus.OK).send({ id: verified.id })
+  } catch (err) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ msg: '' })
   }
 })
 
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,15 @@ router.get('/all', async (req, res) => {
   try {
     res.status(HttpStatus.OK).send(await User.find({}))
   } catch (err) {
-    res.status(HttpStatus.BAD_REQUEST).send(err)
+    res.status(HttpStatus.BAD_REQUEST).send({ msg: err })
+  }
+})
+
+router.delete('/:id/delete', async (req, res) => {
+  try {
+    res.status(HttpStatus.OK).send(await User.deleteOne({ _id: req.params.id }))
+  } catch (err) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ msg: err })
   }
 })
 
