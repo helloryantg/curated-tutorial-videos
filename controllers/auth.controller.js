022644609bce8c59aae04c46748aa60fc85aec5e
helloryantg <helloryantg@gmail.com>
2020-03-05 15:35:50 -0800

    Adds get user route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,7 +6,7 @@ const HttpStatus = require('http-status-codes')
 const createJWT = id => jwt.sign({ id }, process.env.SECRET, { expiresIn: '24h' })
 
 router.post('/register', async (req, res) => {
-  const { 
+  const {
     firstName,
     lastName,
     displayName,
@@ -25,27 +25,35 @@ router.post('/register', async (req, res) => {
 
     await newUser.save()
 
-    res.status(HttpStatus.OK).send({ token: createJWT(newUser.id) })
+    res
+      .status(HttpStatus.OK)
+      .send({ token: createJWT(newUser.id) })
   } catch (err) {
-    res.status(HttpStatus.BAD_REQUEST).send({ msg: err })
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .send({ msg: err })
   }
 })
 
 router.get('/validate/:token', async (req, res) => {
   const { token } = req.params
-  
+
   if (!token) {
-    res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({ msg: 'You must provide a token to authenticate' })
+    res
+      .status(HttpStatus.UNPROCESSABLE_ENTITY)
+      .send({ msg: 'You must provide a token to authenticate' })
   }
 
   try {
     const verified = await jwt.verify(token, process.env.SECRET)
 
-    console.log('id', verified)
-
-    res.status(HttpStatus.OK).send({ id: verified.id })
+    res
+      .status(HttpStatus.OK)
+      .send({ id: verified.id })
   } catch (err) {
-    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ msg: '' })
+    res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send({ msg: '' })
   }
 })
 
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,19 +2,42 @@ const router = require('express').Router()
 const User = require('../models/user.model')
 const HttpStatus = require('http-status-codes')
 
+// Get all users
 router.get('/all', async (req, res) => {
   try {
-    res.status(HttpStatus.OK).send(await User.find({}))
+    res
+      .status(HttpStatus.OK)
+      .send(await User.find({}))
   } catch (err) {
-    res.status(HttpStatus.BAD_REQUEST).send({ msg: err })
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .send({ msg: err })
   }
 })
 
-router.delete('/:id/delete', async (req, res) => {
+// Get user
+router.get('/:id', async (req, res) => {
   try {
-    res.status(HttpStatus.OK).send(await User.deleteOne({ _id: req.params.id }))
+    res
+      .status(HttpStatus.OK)
+      .send(await User.findById(req.params.id))
   } catch (err) {
-    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ msg: err })
+    res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send({ msg: err })
+  }
+})
+
+// Delete user
+router.delete('/:id', async (req, res) => {
+  try {
+    res
+      .status(HttpStatus.OK)
+      .send(await User.deleteOne({ _id: req.params.id }))
+  } catch (err) {
+    res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send({ msg: err })
   }
 })
 
