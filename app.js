9ea2445c02c1d9caa59a7e73808c879eda4107cc
helloryantg <helloryantg@gmail.com>
2020-03-05 13:48:55 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+
+// App
+const app = express()
+
+require('dotenv').config()
+require('./config/database')
+
+// Middleware
+app.use(logger('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json({ type: '*/*' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Routes
+
+
+// Catch all route
+app.get('*', (req, res) => { res.send('404 not found!') })
+
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Listening on port ${port}`))
