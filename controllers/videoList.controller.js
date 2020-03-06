e21b5c1543c581ad572d5bf0bac6175338f5cce4
helloryantg <helloryantg@gmail.com>
2020-03-05 17:17:49 -0800

    Adds create a video list operation

--- controllers/videoList.controller.js
+++ controllers/videoList.controller.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const HttpStatus = require('http-status-codes')
 const authMiddleware = require('../middlewares/auth')
 const VideoList = require('../models/videoList.model')
+const User = require('../models/user.model')
 
 // Get all video lists
 router.get('/all', authMiddleware, async (req, res) => {
@@ -17,5 +18,36 @@ router.get('/all', authMiddleware, async (req, res) => {
 })
 
 // Create a video list
+router.post('/', authMiddleware, async (req, res) => {
+  const { 
+    name, 
+    userId 
+  } = req.body
+  
+  try {
+    const foundUser = User.findOne({ _id: userId })
+
+    if (!foundUser) {
+      res
+        .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .send({ msg: 'You must provide a valid user id' })
+    }
+
+    const newVideoList = new VideoList({
+      name,
+      userId
+    })
+
+    await newVideoList.save()
+
+    res
+      .status(HttpStatus.OK)
+      .send(newVideoList.id)
+  } catch (err) {
+    res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send({ msg: err })
+  }
+})
 
 module.exports = router
