8ae9918a03b9ac8768f6aadf866b64a18f7a18ed
helloryantg <helloryantg@gmail.com>
2020-03-07 10:53:25 -0800

    Adds hidden input

--- client/src/pages/VideoWorkspace/VideoWorkspace.jsx
+++ client/src/pages/VideoWorkspace/VideoWorkspace.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './VideoWorkspace.scss'
 import NavBar from '../../components/NavBar/NavBar'
 
 function VideoWorkspace(props) {
+  const [isAdding, setAdding] = useState(false)
+
   return (
     <div className="VideoWorkspace">
       <NavBar />
@@ -10,11 +12,24 @@ function VideoWorkspace(props) {
         <div className="tabs">
           <div className="title">My Lists</div>
           {/* List goes here */}
+          <div className="add-container">
+            <button
+              onClick={() => setAdding(!isAdding)}
+            >Add a new video list</button>
+            <div className={`hidden ${isAdding ? 'active' : ''}`}>
+              <input type="text" />
+              <button
+                onClick={() => {
+                  setAdding(false)
+                }}
+              >Add</button>
+            </div>
+          </div>
         </div>
 
         <div className="videos-list">
-          
-        </div>  
+
+        </div>
       </div>
     </div>
   )
