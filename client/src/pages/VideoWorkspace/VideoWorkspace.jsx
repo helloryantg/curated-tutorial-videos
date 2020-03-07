33859fd07bbf90ae11db697e281ee9e3add7e41e
helloryantg <helloryantg@gmail.com>
2020-03-06 19:11:21 -0800

    Adds VideoWorkspace

---
+++ client/src/pages/VideoWorkspace/VideoWorkspace.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './VideoWorkspace.scss'
+import NavBar from '../../components/NavBar/NavBar'
+
+function VideoWorkspace(props) {
+  return (
+    <div className="VideoWorkspace">
+      <NavBar />
+    </div>
+  )
+}
+
+export default VideoWorkspace
