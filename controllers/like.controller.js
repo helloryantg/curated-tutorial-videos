485323ac912956f982a1346ba633d0c12fb9469f
helloryantg <helloryantg@gmail.com>
2020-03-09 15:52:50 -0700

    Adds create a like operation

--- controllers/like.controller.js
+++ controllers/like.controller.js
@@ -8,9 +8,36 @@ router.get('/all', authMiddleware, async (req, res) => {
   try {
     res
       .status(HttpStatus.OK)
+      .send(await Like.find({}))
   } catch (err) {
     res
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
       .send({ msg: err })
   }
 })
+
+// Create a like
+router.post('/', authMiddleware, async (req, res) => {
+  const {
+    videoId
+  } = req.body
+  try {
+    const newLike = new Like({
+      userId: req.userId,
+      videoId
+    })
+
+    await newLike.save()
+
+    res
+      .status(HttpStatus.OK)
+      .send(newLike.id)
+  } catch (err) {
+    res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send({ msg: err })
+  }
+})
+
+
+module.exports = router
