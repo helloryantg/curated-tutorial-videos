4dab3e166996d870827e7bac28ad572cf1182a68
helloryantg <helloryantg@gmail.com>
2020-03-09 15:24:39 -0700

    Adds react icons dependency

--- client/src/components/VideoCard/VideoCard.jsx
+++ client/src/components/VideoCard/VideoCard.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './VideoCard.scss'
 import YouTube from 'react-youtube'
+import {
+  IoIosHeart,
+  IoIosHeartEmpty
+} from 'react-icons/io'
 
 function VideoCard(props) {
   const {
@@ -12,7 +16,7 @@ function VideoCard(props) {
   } = props.video
 
   const videoId = url.split('=')[1]
-  
+
   const opts = {
     // height: '160',
     // width: '200',
@@ -28,17 +32,21 @@ function VideoCard(props) {
   return (
     <div className="VideoCard">
       <div className="video">
-        <YouTube 
+        <YouTube
           videoId={videoId}
           opts={opts}
           onReady={onReady}
         />
       </div>
       <div className="description">
-        <div className="title">{title}</div>
-        <div className="favorite">Favorite? {isFavorited ? 'yes' : 'no'}</div>
-        <div className="likes">Likes: {likes}</div>
-        <div className="views">Views: {views}</div>
+        <div className="header">
+          <div className="title">{title}</div>
+          <div className="favorite">{isFavorited ? <IoIosHeart /> : <IoIosHeartEmpty />}</div>
+        </div>
+        <div className="sub-header">
+          <div className="likes">Likes: {likes}</div>
+          <div className="views">Views: {views}</div>
+        </div>
       </div>
     </div>
   )
