9f57e87df19d85f5962dd8d0d1d4fcd01ef08e00
helloryantg <helloryantg@gmail.com>
2020-03-13 21:44:38 -0700

    Adds ability to delete all likes when a video is deleted

--- controllers/video.controller.js
+++ controllers/video.controller.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const HttpStatus = require('http-status-codes')
 const authMiddleware = require('../middlewares/auth')
 const Video = require('../models/video.model')
+const Like = require('../models/like.model')
 
 // Get all videos
 router.get('/all', authMiddleware, async (req, res) => {
@@ -97,9 +98,15 @@ router.put('/:id', authMiddleware, async (req, res) => {
 // Deletes a video
 router.delete('/:id', authMiddleware, async (req, res) => {
   try {
+    const videoId = req.params.id
+    
+    // Deletes all likes associated with the video
+    await Like.deleteMany({ videoId })
+
+    // Deletes the video
     res
       .status(HttpStatus.NO_CONTENT)
-      .send(await Video.findOneAndDelete({ _id: req.params.id }))
+      .send(await Video.findOneAndDelete({ _id: videoId }))
   } catch (err) {
     res
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
