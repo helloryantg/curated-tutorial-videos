60356fa4de65364c8e6a660b75420914e18151c9
helloryantg <helloryantg@gmail.com>
2020-03-21 08:53:02 -0700

    Adds updating state to deleting a video

--- client/src/actions/video.action.js
+++ client/src/actions/video.action.js
@@ -3,7 +3,7 @@
 // import videoListService from '../services/videoList.service'
 import videoService from '../services/video.service'
 // Constants
-// import VIDEO_LIST_CONSTANTS from '../constants/videoList.constants'
+import VIDEO_LIST_CONSTANTS from '../constants/videoList.constants'
 
 const createVideo = video => async dispatch => {
   const data = await videoService.createVideo(video)
@@ -17,8 +17,15 @@ const editVideo = video => async dispatch => {
   // TODO - update the list 
 }
 
-const deleteVideo = video => async dispatch => {
+const deleteVideo = video => async (dispatch, getState) => {
   await videoService.deleteVideo(video)
+
+  const { videos } = getState().reducers
+  
+  dispatch({
+    type: VIDEO_LIST_CONSTANTS.SET_VIDEOS_IN_VIDEO_LIST,
+    payload: videos.filter(vid => vid._id !== video._id)
+  })
 }
 
 export default {
