21732a25312bbb061c06c3726dbc55237164718b
helloryantg <helloryantg@gmail.com>
2020-03-22 09:53:01 -0700

    Refactors the Login page

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -1,21 +1,22 @@
+// React
 import React, { useState } from 'react'
+// Styles
 import './AuthPage.scss'
+// Components
 import Login from '../../components/Login/Login'
 import SignUp from '../../components/SignUp/SignUp'
 
 function AuthPage(props) {
   const [isLoginPage, setLoginPage] = useState(true)
 
-  // TODO add signup page
   return (
     <div className="AuthPage">
       <div className="container">
         <div className="logo-container">
-          <div className="top">
-            <div className="title">Curated Tutorial Videos</div>
-          </div>
-          <div className="bottom"></div>
+          <div className="title">Curated Tutorial Videos</div>
+          {/* <div className="bottom"></div> */}
         </div>
+
         <div className="forms-container">
           {isLoginPage ? 
             <Login 
