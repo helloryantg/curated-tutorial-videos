47d1298a87d2ef553fd379fcb5343b2141f72fde
helloryantg <helloryantg@gmail.com>
2020-03-22 12:16:26 -0700

    Creates custom Input component

---
+++ client/src/components/Input/Input.jsx
@@ -0,0 +1,31 @@
+// React
+import React from 'react'
+// Styles
+import './Input.scss'
+// Utils
+import stringUtils from '../../utils/string.utils'
+
+function Input(props) {
+  const { 
+    option,
+    placeholder = '',
+    value,
+    onChange,
+    error
+  } = props
+
+  return (
+    <div className="Input">
+      <label>{stringUtils.camelCaseToTitleCase(option)}</label>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={({ target }) => onChange(target)}
+      />
+      {error ? <div className="error">{error}</div> : null}
+    </div>
+  )
+}
+
+export default Input
