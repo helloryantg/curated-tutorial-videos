690edaf8430783ea4f6d7e4db82b6a8f62d74724
Ryan Gonzales <helloryantg@gmail.com>
2020-04-17 22:30:51 -0700

    Fixes the broken url page

--- client/src/components/VideoCard/VideoCard.jsx
+++ client/src/components/VideoCard/VideoCard.jsx
@@ -1,5 +1,5 @@
 // React
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // Redux
 import { connect } from 'react-redux'
 // Styles
@@ -33,7 +33,7 @@ function VideoCard(props) {
     likesArray
   } = video
 
-  const videoId = videoUtils(url)
+  const videoId = videoUtils.getVideoIdFromUrl(url)
 
   const [isFavorited, setFavorite] = useState(likesArray.some(like => like.userId === user._id))
   const [likesCount, setLikesCount] = useState(likesArray.length)
