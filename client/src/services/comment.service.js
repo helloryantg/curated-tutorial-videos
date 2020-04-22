027477c2466b8eb4ed4108069e863c43e1765d7b
helloryantg <helloryantg@gmail.com>
2020-04-21 22:51:56 -0700

    Creates comment service

---
+++ client/src/services/comment.service.js
@@ -0,0 +1,32 @@
+import { server } from '../interfaces/axios.interface'
+
+// Untested
+const createComment = comment => {
+  return server.post('/comments', comment)
+    .then(({ data }) => data)
+}
+
+// Untested
+const editComment = comment => {
+  return server.put(`/comments/${comment._id}`, comment)
+    .then(({ data }) => data)
+}
+
+// Untested
+const getComment = id => {
+  return server.get(`'/videos/${id}`)
+    .then(({ data }) => data)
+}
+
+// Untested
+const deleteComment = id => {
+  return server.delete(`/videos/${id}`)
+    .then(({ data }) => data)
+}
+
+export default {
+  createComment,
+  editComment,
+  getComment,
+  deleteComment
+}
