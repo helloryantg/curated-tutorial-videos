011f3617669d61caa78d9861b16853242b4df081
Ryan Gonzales <helloryantg@gmail.com>
2020-05-06 22:48:00 -0700

    Adds search bar on Search Page

--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -4,10 +4,13 @@ import React, { useState } from 'react'
 import './SearchPage.scss'
 
 function SearchPage(props) {
+  const [searchText, setSearchText] = useState('')
 
   return (
     <div className="SearchPage">
-      SearchPage
+      <div className="search-bar">
+        <input type="text" value={searchText} onChange={({ target }) => setSearchText(target.value)} />
+      </div>
     </div>
   )
 }
