0ff3c25a5a3a36e39df621c2c48a2382c34fff6a
Ryan Gonzales <helloryantg@gmail.com>
2020-05-09 20:11:09 -0700

    Creates the Tab component

---
+++ client/src/components/Tab/Tab.jsx
@@ -0,0 +1,14 @@
+// React
+import React from 'react'
+// Styles
+import './Tab.scss'
+
+function Tab(props) {
+  return (
+    <div className="Tab">
+      Tab
+    </div>
+  )
+}
+
+export default Tab
