c474ce0c28fff75ae1ec8b8ed44a114bc04190e0
helloryantg <helloryantg@gmail.com>
2020-05-10 12:10:34 -0700

    Adds search input into the nav bar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 // React
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 // Redux
 import { connect } from 'react-redux'
@@ -9,12 +9,20 @@ import './NavBar.scss'
 import { isEmpty } from '../../utils/object'
 // Actions
 import userActions from '../../actions/user.action'
+// Dependencies
+import { 
+  IoMdSearch
+} from "react-icons/io"
+
 
 function NavBar(props) {
   const { 
     user,
     dispatch
   } = props
+
+  const [searchText, setSearchText] = useState('')
+
   const initials = !isEmpty(user) && user.firstName[0] + user.lastName[0]
   
   return (
@@ -22,7 +30,12 @@ function NavBar(props) {
       <Link to="/" className="logo">
         CTV
       </Link>
-      <div className="search"></div>
+      <div className="search">
+        <input type="text" value={searchText} onChange={({ target }) => setSearchText(target.value)} />
+        <button>
+          <IoMdSearch />
+        </button>
+      </div>
       <div className="user">
         <div className="initials">{initials}</div>
         <Link 
