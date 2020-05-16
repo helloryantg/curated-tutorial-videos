32409f1c3e9860cd9d4994482bcd133a0eec6eff
helloryantg <helloryantg@gmail.com>
2020-05-15 23:32:19 -0700

    Adds video lists to tab

--- client/src/components/Tab/Tab.jsx
+++ client/src/components/Tab/Tab.jsx
@@ -1,16 +1,25 @@
 // React
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 // Styles
 import './Tab.scss'
 // Dependencies
 import { 
   IoMdSearch,
   IoIosList,
+  IoIosArrowForward
 } from "react-icons/io"
+// Utils
+import { isEmpty } from '../../utils/object'
+// Actions
+import videoListAction from '../../actions/videoList.action'
 
 function Tab(props) {
   const {
-    setCurrentTab
+    setCurrentTab,
+    user,
+    dispatch,
+    videoLists
   } = props
 
   const tabs = [
@@ -18,10 +27,32 @@ function Tab(props) {
     { label: 'My Lists', icon: IoIosList },
   ]
 
+  const [allTabs, setAllTabs] = useState([...tabs])
+
+  useEffect(() => {
+    if (!isEmpty(user)) {
+      dispatch(videoListAction.getUserVideoLists(user._id))
+    }
+  }, [user, dispatch])
+
+  useEffect(() => {
+    const userTabs = videoLists.map(list => {
+      return {
+        label: list.name,
+        icon: IoIosArrowForward,
+        list,
+      }
+    })
+
+    setAllTabs([...tabs, ...userTabs])
+  }, [videoLists])
+
+  console.log(videoLists)
+
   return (
     <div className="Tab">
       <div className="tabs">
-        {tabs.map((tab, index) => (
+        {allTabs.map((tab, index) => (
           <div 
             className="tab" 
             key={`${tab.label}-${index}`}
@@ -36,4 +67,9 @@ function Tab(props) {
   )
 }
 
-export default Tab
+const mapState = ({ reducers }) => ({
+  videoLists: reducers.videoLists,
+  user: reducers.user,
+})
+
+export default connect(mapState)(Tab)
