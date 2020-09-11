5b9aebd75c2336e339394624df4c13bf3ea60a47
helloryantg <helloryantg@gmail.com>
2020-09-10 20:20:34 -0700

    Remove one last serviceWorker line

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './components/App/App'
-import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
