24d603d3845b819736470c895fc46a31cb722c00
helloryantg <helloryantg@gmail.com>
2020-10-09 22:14:04 -0700

    add get all users endpoint

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const User = require('../models/user.model')
 const HttpStatus = require('http-status-codes')
 const authMiddleware = require('../middlewares/auth')
+const Video = require('../models/video.model')
 
 // Get all users
 router.get('/all', authMiddleware, async (req, res) => {
@@ -38,6 +39,17 @@ router.delete('/:id', async (req, res) => {
   } catch (err) {
     res
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send({ msg: err })
+  }
+})
+
+// Get all user videos
+router.get('/:id/videos', async (req, res) => {
+  try {
+    res.status(HttpStatus.OK)
+      .send(await Video.find({ userId: req.params.id }))
+  } catch (err) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR)
       .send({ msg: err })
   }
 })
